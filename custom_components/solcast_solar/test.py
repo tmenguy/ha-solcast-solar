71abb280c58fbc28307ca4c26ebea7db687c1e44
Steve Saunders <steve@map59.com>
2024-08-21 19:59:33 +1000

    Update test.py

--- custom_components/solcast_solar/test.py
+++ custom_components/solcast_solar/test.py
@@ -3,22 +3,39 @@
 import asyncio
 import logging
 import traceback
+from .const import SOLCAST_URL
+from homeassistant.util import dt as dt_util
 
-from aiohttp import ClientConnectionError, ClientSession
+from aiohttp import ClientSession
 
 from .solcastapi import ConnectionOptions, SolcastApi
 
-#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 _LOGGER = logging.getLogger(__name__)
 
 
 async def test():
+    print('This script is for development purposes only')
     try:
-        
+        optdamp = {}
+        for a in range(0,24): optdamp[str(a)] = 1.0
+
         options = ConnectionOptions(
-            "changetoyourapikey",
-            "https://api.solcast.com.au",
-            'solcast.json'
+            "apikeygoeshere",
+            SOLCAST_URL,
+            'solcast.json',
+            "/config",
+            await dt_util.async_get_time_zone(hass.config.time_zone),
+            optdamp,
+            1,
+            "estimate",
+            100,
+            True,
+            True,
+            True,
+            True,
+            True,
+            True
         )
         
         async with ClientSession() as session:
