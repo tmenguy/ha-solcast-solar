ced1f481f50a7a33d377368fff3785ebb72a73f9
Steve Saunders <steve@map59.com>
2024-11-07 17:19:37 +1100

    Improved options flow #215, #216

--- custom_components/solcast_solar/config_flow.py
+++ custom_components/solcast_solar/config_flow.py
@@ -85,49 +85,28 @@ class SolcastSolarFlowHandler(ConfigFlow, domain=DOMAIN):
             return self.async_abort(reason="single_instance_allowed")
 
         if user_input is not None:
+            options = {
+                CONF_API_KEY: user_input[CONF_API_KEY],
+                API_QUOTA: user_input[API_QUOTA],
+                AUTO_UPDATE: int(user_input[AUTO_UPDATE]),
+                # Remaining options set to default
+                CUSTOM_HOUR_SENSOR: 1,
+                HARD_LIMIT_API: '100.0',
+                KEY_ESTIMATE: "estimate",
+                BRK_ESTIMATE: True,
+                BRK_ESTIMATE10: True,
+                BRK_ESTIMATE90: True,
+                BRK_SITE: True,
+                BRK_HALFHOURLY: True,
+                BRK_HOURLY: True,
+                BRK_SITE_DETAILED: False,
+            }
+            damp = {f"damp{factor:02d}": 1.0 for factor in range(24)}
+
             return self.async_create_entry(
-                title= TITLE,
+                title = TITLE,
                 data = {},
-                options={
-                    CONF_API_KEY: user_input[CONF_API_KEY],
-                    API_QUOTA: user_input[API_QUOTA],
-                    AUTO_UPDATE: int(user_input[AUTO_UPDATE]),
-                    # Remaining options set to default
-                    "damp00": 1.0,
-                    "damp01": 1.0,
-                    "damp02": 1.0,
-                    "damp03": 1.0,
-                    "damp04": 1.0,
-                    "damp05": 1.0,
-                    "damp06": 1.0,
-                    "damp07": 1.0,
-                    "damp08": 1.0,
-                    "damp09": 1.0,
-                    "damp10": 1.0,
-                    "damp11": 1.0,
-                    "damp12": 1.0,
-                    "damp13": 1.0,
-                    "damp14": 1.0,
-                    "damp15": 1.0,
-                    "damp16": 1.0,
-                    "damp17": 1.0,
-                    "damp18": 1.0,
-                    "damp19": 1.0,
-                    "damp20": 1.0,
-                    "damp21": 1.0,
-                    "damp22": 1.0,
-                    "damp23": 1.0,
-                    "customhoursensor": 1,
-                    HARD_LIMIT_API: '100.0',
-                    KEY_ESTIMATE: "estimate",
-                    BRK_ESTIMATE: True,
-                    BRK_ESTIMATE10: True,
-                    BRK_ESTIMATE90: True,
-                    BRK_SITE: True,
-                    BRK_HALFHOURLY: True,
-                    BRK_HOURLY: True,
-                    BRK_SITE_DETAILED: False,
-                },
+                options = options | damp,
             )
 
         solcast_json_exists = file_exists(f"{os.path.abspath(os.path.join(os.path.dirname(__file__) ,'../..'))}/solcast.json")
@@ -161,8 +140,8 @@ class SolcastSolarOptionFlowHandler(OptionsFlow):
         Arguments:
             entry (ConfigEntry): The integration entry instance, contains the configuration.
         """
-        self.config_entry = entry
-        self.options = dict(self.config_entry.options)
+        self._entry = entry
+        self._options = entry.options
 
     async def async_step_init(self, user_input: dict=None) -> Any:
         """Initialise main dialogue step.
@@ -175,24 +154,24 @@ class SolcastSolarOptionFlowHandler(OptionsFlow):
         """
 
         errors = {}
-        api_key = self.config_entry.options.get(CONF_API_KEY)
-        api_quota = self.config_entry.options[API_QUOTA]
-        auto_update = self.config_entry.options[AUTO_UPDATE]
-        custom_hour_sensor = self.config_entry.options[CUSTOM_HOUR_SENSOR]
-        hard_limit = self.config_entry.options.get(HARD_LIMIT_API)
-        key_estimate = self.config_entry.options.get(KEY_ESTIMATE, "estimate")
-        estimate_breakdown = self.config_entry.options[BRK_ESTIMATE]
-        estimate_breakdown10 = self.config_entry.options[BRK_ESTIMATE10]
-        estimate_breakdown90 = self.config_entry.options[BRK_ESTIMATE90]
-        site_breakdown = self.config_entry.options[BRK_SITE]
-        half_hourly = self.config_entry.options[BRK_HALFHOURLY]
-        hourly = self.config_entry.options[BRK_HOURLY]
-        site_detailed = self.config_entry.options[BRK_SITE_DETAILED]
-        granular_dampening = self.config_entry.options[SITE_DAMP]
+        api_key = self._options.get(CONF_API_KEY)
+        api_quota = self._options[API_QUOTA]
+        auto_update = self._options[AUTO_UPDATE]
+        custom_hour_sensor = self._options[CUSTOM_HOUR_SENSOR]
+        hard_limit = self._options.get(HARD_LIMIT_API)
+        key_estimate = self._options.get(KEY_ESTIMATE, "estimate")
+        estimate_breakdown = self._options[BRK_ESTIMATE]
+        estimate_breakdown10 = self._options[BRK_ESTIMATE10]
+        estimate_breakdown90 = self._options[BRK_ESTIMATE90]
+        site_breakdown = self._options[BRK_SITE]
+        half_hourly = self._options[BRK_HALFHOURLY]
+        hourly = self._options[BRK_HOURLY]
+        site_detailed = self._options[BRK_SITE_DETAILED]
+        granular_dampening = self._options[SITE_DAMP]
 
         if user_input is not None:
             try:
-                all_config_data = {**self.config_entry.options}
+                all_config_data = {**self._options}
 
                 api_key = user_input["api_key"].replace(" ","")
                 api_key = [s for s in api_key.split(',') if s]
@@ -248,7 +227,7 @@ class SolcastSolarOptionFlowHandler(OptionsFlow):
                     all_config_data[SITE_DAMP] = user_input[SITE_DAMP]
 
                 self.hass.config_entries.async_update_entry(
-                    self.config_entry,
+                    self._entry,
                     title=TITLE,
                     options=all_config_data,
                 )
@@ -314,88 +293,17 @@ class SolcastSolarOptionFlowHandler(OptionsFlow):
         """
 
         errors = {}
-
-        damp00 = self.config_entry.options["damp00"]
-        damp01 = self.config_entry.options["damp01"]
-        damp02 = self.config_entry.options["damp02"]
-        damp03 = self.config_entry.options["damp03"]
-        damp04 = self.config_entry.options["damp04"]
-        damp05 = self.config_entry.options["damp05"]
-        damp06 = self.config_entry.options["damp06"]
-        damp07 = self.config_entry.options["damp07"]
-        damp08 = self.config_entry.options["damp08"]
-        damp09 = self.config_entry.options["damp09"]
-        damp10 = self.config_entry.options["damp10"]
-        damp11 = self.config_entry.options["damp11"]
-        damp12 = self.config_entry.options["damp12"]
-        damp13 = self.config_entry.options["damp13"]
-        damp14 = self.config_entry.options["damp14"]
-        damp15 = self.config_entry.options["damp15"]
-        damp16 = self.config_entry.options["damp16"]
-        damp17 = self.config_entry.options["damp17"]
-        damp18 = self.config_entry.options["damp18"]
-        damp19 = self.config_entry.options["damp19"]
-        damp20 = self.config_entry.options["damp20"]
-        damp21 = self.config_entry.options["damp21"]
-        damp22 = self.config_entry.options["damp22"]
-        damp23 = self.config_entry.options["damp23"]
+        extant = {f"damp{factor:02d}": self._options[f"damp{factor:02d}"] for factor in range(24)}
 
         if user_input is not None:
             try:
-                damp00 = user_input["damp00"]
-                damp01 = user_input["damp01"]
-                damp02 = user_input["damp02"]
-                damp03 = user_input["damp03"]
-                damp04 = user_input["damp04"]
-                damp05 = user_input["damp05"]
-                damp06 = user_input["damp06"]
-                damp07 = user_input["damp07"]
-                damp08 = user_input["damp08"]
-                damp09 = user_input["damp09"]
-                damp10 = user_input["damp10"]
-                damp11 = user_input["damp11"]
-                damp12 = user_input["damp12"]
-                damp13 = user_input["damp13"]
-                damp14 = user_input["damp14"]
-                damp15 = user_input["damp15"]
-                damp16 = user_input["damp16"]
-                damp17 = user_input["damp17"]
-                damp18 = user_input["damp18"]
-                damp19 = user_input["damp19"]
-                damp20 = user_input["damp20"]
-                damp21 = user_input["damp21"]
-                damp22 = user_input["damp22"]
-                damp23 = user_input["damp23"]
-
-                all_config_data = {**self.config_entry.options}
-                all_config_data["damp00"] = damp00
-                all_config_data["damp01"] = damp01
-                all_config_data["damp02"] = damp02
-                all_config_data["damp03"] = damp03
-                all_config_data["damp04"] = damp04
-                all_config_data["damp05"] = damp05
-                all_config_data["damp06"] = damp06
-                all_config_data["damp07"] = damp07
-                all_config_data["damp08"] = damp08
-                all_config_data["damp09"] = damp09
-                all_config_data["damp10"] = damp10
-                all_config_data["damp11"] = damp11
-                all_config_data["damp12"] = damp12
-                all_config_data["damp13"] = damp13
-                all_config_data["damp14"] = damp14
-                all_config_data["damp15"] = damp15
-                all_config_data["damp16"] = damp16
-                all_config_data["damp17"] = damp17
-                all_config_data["damp18"] = damp18
-                all_config_data["damp19"] = damp19
-                all_config_data["damp20"] = damp20
-                all_config_data["damp21"] = damp21
-                all_config_data["damp22"] = damp22
-                all_config_data["damp23"] = damp23
+                all_config_data = {**self._options}
+                for factor in range(24):
+                    all_config_data[f"damp{factor:02d}"] = user_input[f"damp{factor:02d}"]
                 all_config_data[SITE_DAMP] = False
 
                 self.hass.config_entries.async_update_entry(
-                    self.config_entry,
+                    self._entry,
                     title=TITLE,
                     options=all_config_data,
                 )
@@ -408,54 +316,9 @@ class SolcastSolarOptionFlowHandler(OptionsFlow):
             step_id="dampen",
             data_schema=vol.Schema(
                 {
-                    vol.Required("damp00", description={"suggested_value": damp00}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp01", description={"suggested_value": damp01}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp02", description={"suggested_value": damp02}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp03", description={"suggested_value": damp03}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp04", description={"suggested_value": damp04}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp05", description={"suggested_value": damp05}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp06", description={"suggested_value": damp06}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp07", description={"suggested_value": damp07}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp08", description={"suggested_value": damp08}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp09", description={"suggested_value": damp09}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp10", description={"suggested_value": damp10}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp11", description={"suggested_value": damp11}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp12", description={"suggested_value": damp12}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp13", description={"suggested_value": damp13}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp14", description={"suggested_value": damp14}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp15", description={"suggested_value": damp15}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp16", description={"suggested_value": damp16}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp17", description={"suggested_value": damp17}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp18", description={"suggested_value": damp18}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp19", description={"suggested_value": damp19}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp20", description={"suggested_value": damp20}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp21", description={"suggested_value": damp21}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp22", description={"suggested_value": damp22}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
-                    vol.Required("damp23", description={"suggested_value": damp23}):
-                            vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0)),
+                    vol.Required(f"damp{factor:02d}", description={"suggested_value": extant[f"damp{factor:02d}"]}):
+                        vol.All(vol.Coerce(float), vol.Range(min=0.0,max=1.0))
+                    for factor in range(24)
                 }
             ),
             errors=errors,
