ebd356ad8053fb775b4d52437fb5d0859d9ab254
Steve Saunders <steve@map59.com>
2024-11-21 22:02:56 +1100

    Startup check for conflicting integrations

--- custom_components/solcast_solar/__init__.py
+++ custom_components/solcast_solar/__init__.py
@@ -5,6 +5,7 @@ import contextlib
 from datetime import timedelta
 import json
 import logging
+from pathlib import Path
 import random
 import traceback
 from typing import Any, Final
@@ -288,6 +289,22 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:  #
 
     """
     random.seed()
+
+    def find_others() -> list[str]:
+        path = Path(Path.cwd(), "..")
+        return list(path.rglob("solcastapi.py"))
+
+    others = await hass.async_add_executor_job(find_others)
+    for sol in others:
+        parent = str(Path(sol).parent)
+        folder = parent.rsplit("/", maxsplit=1)
+        if folder != "solcast_solar":
+            async with aiofiles.open(Path(parent, "manifest.json")) as file:
+                manifest = json.loads(await file.read())
+                if manifest.get("domain") == folder:
+                    _LOGGER.debug("Competitor found: %s", sol)
+                    raise ConfigEntryNotReady(f"Load failed: Conflicting integration found: {folder}")
+
     version = await __get_version(hass)
     options = await __get_options(hass, entry)
     __setup_storage(hass)
